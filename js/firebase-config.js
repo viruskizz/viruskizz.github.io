3babed9cf4936e034b115dd2b24ed6c502c89b86
viruskizz <kizzaraiva@gmail.com>
2025-03-07 13:47:14 +0700

    add analytic

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
+import {firebaseConfig} from './firebase.credential.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Initialize Firebase
+console.log(firebaseConfig)
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
